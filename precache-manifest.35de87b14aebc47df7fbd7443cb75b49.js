bbeb20a8efb0ca39afa5e61070a55b704511aa5d
luonglearnstocode <luong.nguyen@metropolia.fi>
2018-11-28 07:58:39 +0200

    Updates

--- precache-manifest.35de87b14aebc47df7fbd7443cb75b49.js
+++ precache-manifest.35de87b14aebc47df7fbd7443cb75b49.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/vr-dict/static/js/runtime~main.cf6652f9.js"
   },
   {
-    "revision": "7fbdc1dc959cb6e83b1d",
-    "url": "/vr-dict/static/js/main.7fbdc1dc.chunk.js"
+    "revision": "9e57f8e8244fe1098900",
+    "url": "/vr-dict/static/js/main.9e57f8e8.chunk.js"
   },
   {
     "revision": "1cf1dbc1c782536061f7",
     "url": "/vr-dict/static/js/1.1cf1dbc1.chunk.js"
   },
   {
-    "revision": "7fbdc1dc959cb6e83b1d",
-    "url": "/vr-dict/static/css/main.604e114a.chunk.css"
+    "revision": "9e57f8e8244fe1098900",
+    "url": "/vr-dict/static/css/main.1bda2642.chunk.css"
   },
   {
-    "revision": "0e51813e700d5ddd33012ef0b8c39085",
+    "revision": "9374883fe9e980722052744dccba9bc5",
     "url": "/vr-dict/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vr-dict/precache-manifest.690bb6227e0ecdf6b7a79dc6e6ba4902.js"
+  "/vr-dict/precache-manifest.35de87b14aebc47df7fbd7443cb75b49.js"
 );
 
 workbox.clientsClaim();
